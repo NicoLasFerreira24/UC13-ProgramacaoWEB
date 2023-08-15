ea956dad316e844c325c6d23be2b00fdfabdfa46
Nicolas Ferreira <134647986+NicoLasFerreira24@users.noreply.github.com>
2023-08-15 15:25:38 -0300

    Variaveis e Funções

---
+++ variaveis.php
@@ -0,0 +1,42 @@
+<?php
+
+$nomeCompleto = 'Nicolas Ferreira';
+$idade = 24;
+$status = true;
+$salario = 1500.00;
+
+echo $nomeCompleto;
+echo "<hr>";
+
+// Converte em tudo maisculo.
+echo strtoupper($nomeCompleto);
+
+// Pular linha
+echo "<hr>";
+
+// Converte um tudo minusculo.
+echo strtolower($nomeCompleto);
+
+echo "<hr>";
+
+// Converte em maiscula a primeira letra de cada palavra.
+echo ucwords($nomeCompleto);
+
+echo "<hr>";
+
+// Procura a palavra, altere, e depois a variavel que se encontra.
+echo str_replace("Nicolas", "Nicholas", $nomeCompleto);
+
+echo "<hr>";
+
+// Repete a palavra, como se fosse um sistema de repetição.
+echo str_repeat("Nicolas, ", 5);
+
+echo "<hr>";
+
+// Verifica de tem a variavel existe.
+echo isset($nome) ? 'True' : 'False';
+
+echo "<hr>";
+
+echo empty($nome) ? 'True' : 'False';
