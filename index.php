248160f18140d1ee585f2b86ad4efad469a7c346
Nicolas Ferreira <134647986+NicoLasFerreira24@users.noreply.github.com>
2023-08-29 17:17:47 -0300

    Aula de Bootstrap

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+    <div class="container">
+
+        <div class="text-bg-primary pb-1 mt-2 rounded-3 text-center">
+
+            <h1>Aula de Bootstrap</h1>
+
+        </div>
+
+        <div class="row">
+            <div class="bg-black text-white col-12 col-md-4 ">
+                <h2>Coluna 1</h2>
+            </div>
+
+            <div class="bg-danger text-white col-6 col-md-4 ">
+                <h2>Coluna 2</h2>
+            </div>
+
+            <div class= "bg-dark-subtle col-6 col-md-4 ">
+                <h2>Coluna 3</h2>
+            </div>
+
+        </div>
+
+        <div class= "row">
+
+            <div class="bg-success text-white col-12 col-md-12 ">
+                <h2>Coluna 4</h2>
+            </div>
+
+            <div class="bg-warning text-white col-12 col-md-6 ">
+                <h2>Coluna 5</h2>
+            </div>
+
+            <div class= "bg-secondary col-6 d-none d-md-block ">
+                <h2>Coluna 6</h2>
+            </div>
+
+            <div class="bg-info text-white col-12 col-md-12">
+                <h2>Coluna 7</h2>
+            </div>
+
+        </div>
+
+       
+        
+
+    </div>
+
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
